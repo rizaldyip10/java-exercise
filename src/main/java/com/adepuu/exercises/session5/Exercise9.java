135902f5df79396577cff99ee3482c6d97fee81b
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-03-25 20:19:34 +0700

    exercise 9

--- src/main/java/com/adepuu/exercises/session5/Exercise9.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise9.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Arrays;
+
 public class Exercise9 {
     /**
      * Java Program to get the number of days you have to wait after the i-th day to get a warmer temperature
@@ -17,6 +19,24 @@ public class Exercise9 {
      * Output: [1,1,0]
      */
     public static void main(String[] args) {
+        int[] temperature = {30,30,60,90};
+        int[] res = calcDay(temperature);
+        System.out.println(Arrays.toString(res));
+    }
 
+    static int[] calcDay(int[] temperature) {
+        int[] result = new int[temperature.length];
+
+        for (int i = 0; i < temperature.length; i++) {
+            int days = 0;
+            for (int j = i + 1; j < temperature.length; j++){
+                days++;
+                if (temperature[j] > temperature[i]) {
+                    result[i] = days;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
