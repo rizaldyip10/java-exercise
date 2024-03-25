6e79f171c6fc12cf0f8bfdb9cb86644fea8988cb
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-03-25 20:18:51 +0700

    exercise 8

--- src/main/java/com/adepuu/exercises/session5/Exercise8.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise8.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercise8 {
     /**
      * Java String Program to Find all Duplicates on Array
@@ -17,6 +20,32 @@ public class Exercise8 {
      * Output: []
      */
     public static void main(String[] args) {
+        int[] nums = {1,2,2,3,3};
+        List<Integer> res = findDuplicate(nums);
+        System.out.println(res);
+    }
 
+    static List<Integer> findDuplicate(int[] numArr) {
+        List<Integer> result = new ArrayList<>();
+        sortNum(numArr);
+
+        for (int i = 0; i < numArr.length - 1; i++) {
+            if (numArr[i] == numArr[i + 1]) {
+                result.add(numArr[i]);
+            }
+        }
+        return result;
+    }
+
+    static void sortNum(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 }
