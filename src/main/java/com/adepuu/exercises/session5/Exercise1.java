33224f1e51074926ff39eef93372ab1d855ee587
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-03-25 10:48:48 +0700

    exercise 1

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,21 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise1 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        int result = largestInt(nums);
+        System.out.println(result);
+    }
+
+    static int largestInt(int[] numArr) {
+        int max = numArr[0];
+
+        for (int num : numArr) {
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        return max;
+    }
 }
