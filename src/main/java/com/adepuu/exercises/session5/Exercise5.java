349656d03864c0deab99230772bfe785ab5fd5cf
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-03-25 20:15:37 +0700

    exercise 5

--- src/main/java/com/adepuu/exercises/session5/Exercise5.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise5.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Arrays;
+
 public class Exercise5 {
     /**
      * Java Array Program to sort array in increasing & decreasing order
@@ -13,6 +15,40 @@ public class Exercise5 {
      * Output: [ 8,7, 5, 2 ]
      */
     public static void main(String[] args) {
+        int[] nums = {1, 2 ,6, 9, 5};
+        sortingArr(nums, "asc");
+        System.out.println(Arrays.toString(nums));
+    }
 
+    static void sortingArr(int[] numArr, String order) {
+        switch (order) {
+            case "desc" -> descOrder(numArr);
+            case "asc" -> ascOrder(numArr);
+            default -> System.out.println("Please define the right sorting order");
+        }
+    }
+
+    static void ascOrder(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static  void descOrder(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 }
