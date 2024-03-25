71559718638ae17f160dad693670e0f46b5b9cba
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-03-25 20:17:02 +0700

    exercise 7

--- src/main/java/com/adepuu/exercises/session5/Exercise7.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise7.java
@@ -13,6 +13,37 @@ public class Exercise7 {
      * Output: false
      */
     public static void main(String[] args) {
+        String s = "car";
+        String t = "rat";
+        boolean result = checkAnagram(s, t);
+        System.out.println(result);
+    }
 
+    static boolean checkAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+
+        sortChar(str1);
+        sortChar(str2);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (str1[i] != str2[i]) return false;
+        }
+
+        return true;
+    }
+
+    static void sortChar(char[] charArr) {
+        for (int i = 0; i < charArr.length - 1; i++) {
+            for (int j = 0; j < charArr.length - i - 1; j++) {
+                if (charArr[j] > charArr[j + 1]) {
+                    char temp = charArr[j];
+                    charArr[j] = charArr[j + 1];
+                    charArr[j + 1] = temp;
+                }
+            }
+        }
     }
 }
